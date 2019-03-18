19cc05e7fcd7223366c0bc4b2dc1f2c5d4786c7a
Andrew Downie <andrew_dev@outlook.com>
2019-03-18 18:41:15 -0400

    ready for tut 4

--- src/Components/Exercises/index.js
+++ src/Components/Exercises/index.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import {Grid} from 'material-ui';
+import React, {Fragment} from 'react';
+import {Grid, Paper, Typography, List} from 'material-ui';
+import {ListItem, ListItemText} from 'material-ui/List';
 
-import LeftPane from './LeftPane';
-import RightPane from './RightPane';
 
 const styles = {
   Paper: {
     padding:20,
     marginTop: 10,
-    marginBottom: 10
+    marginBottom: 10,
+    height: 500,
+    overflowY: 'auto'
   }
 };
 
-export default props => (
+export default ({exercises}) => (
   <Grid container>
     <Grid xs={6} item>
-      <LeftPane styles={styles} />
+      <Paper style={styles.Paper}>
+        {exercises.map(([group, exercises]) => 
+          <Fragment>
+            <Typography variant="headline" style={{textTransform: 'capitalize'}}>
+              {group}
+            </Typography>
+            <List component="ul">
+              {exercises.map(({title}) =>
+                <ListItem button>
+                  <ListItemText primary={title} />
+                </ListItem>
+              )}
+            </List>
+          </Fragment>
+        )}
+      </Paper>
     </Grid>
     <Grid xs={6} item>
-      <RightPane styles={styles} />
+      <Paper style={styles.Paper}>
+        <Typography variant="display1">
+          Welcome
+        </Typography>
+        <Typography variant="subheading" style={{marginTop: 20}}>
+          Please select an exercise from the list on the left
+        </Typography>
+      </Paper>
     </Grid>
   </Grid>
 )
