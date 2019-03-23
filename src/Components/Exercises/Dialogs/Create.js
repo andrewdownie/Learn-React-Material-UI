a7151668089d3b42a779db47186d985773c45946
Andrew Downie <andrew_dev@outlook.com>
2019-03-23 19:47:50 -0400

    ready for tut 7

--- src/Components/Exercises/Dialogs/Create.js
+++ src/Components/Exercises/Dialogs/Create.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react';
-import {Button, Dialog} from 'material-ui';
+import {Button, Dialog, TextField, Select} from 'material-ui';
 import {
   DialogActions,
   DialogContent,
@@ -7,47 +7,133 @@ import {
   DialogTitle
 } from 'material-ui/Dialog';
 import { Add } from 'material-ui-icons';
+import {FormControl} from 'material-ui/Form';
+import {InputLabel} from 'material-ui/Input';
+import {MenuItem} from 'material-ui/Menu';
+import {withStyles} from 'material-ui/styles';
 
-export default class extends Component {
-  state = {
-    open: false
+const styles = theme => ({
+  FormControl: {
+    width: 500
   }
+})
+  
 
-  handleToggle = () => {
-    this.setState({
-      open: !this.state.open
-    });
+export default withStyles(styles)(class extends Component {
+    state = {
+      open: false,
+      exercise: {
+        tile: '',
+        description: '',
+        muscles: ''
+      }
+    }
+
+    handleToggle = () => {
+      this.setState({
+        open: !this.state.open
+      });
+    }
+
+    handleChange = (name) => ({target: {value}}) => {
+      this.setState({
+        exercise: {
+          ...this.state.exercise,
+          [name]: value
+        }
+      });
+    }
+    
+    handleSubmit = () => {
+      //TODO: validation needed
+      
+      const {exercise} = this.state;
+      
+      this.props.onCreate({
+        ...exercise,
+        id: exercise.title.toLowerCase().replace(/ /g, '-')
+      });
+      
+      this.setState({
+        open: false,
+        exercise: {
+          title: '',
+          description: '',
+          muscles: ''
+        }
+      })
+    }
+
+    render(){
+      const {open, exercise: {title, description, muscles}} = this.state,
+            {classes, muscles: categories} = this.props;
+      return(
+          <Fragment>
+            <Button variant="fab" onClick={this.handleToggle} mini>
+              <Add/>
+            </Button>
+
+            <Dialog
+              open={open}
+              onClose={this.handleToggle}
+            >
+              <DialogTitle id="form-dialog-title">
+                Create a new exercise
+              </DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Please fill out the form below
+                </DialogContentText>
+                <form>
+                  <TextField
+                    label="Title"
+                    value={title}
+                    onChange={this.handleChange('title')}
+                    margin="normal"
+                    className={classes.FormControl}
+                  />
+                  <br/>
+                  <FormControl className={classes.FormControl}>
+                    <InputLabel htmlFor="muscles">
+                      Muscles
+                    </InputLabel>
+                    <Select
+                      value={muscles}
+                      onChange={this.handleChange('muscles')}
+                    >
+                      {categories.map(category => 
+                        <MenuItem key={category} value={category}>
+                          {category}
+                        </MenuItem>
+                      )}
+
+                    </Select>
+                  </FormControl>
+                 <br/> 
+                  <TextField
+                    multiline
+                    rows="4"
+                    label="Description"
+                    value={description}
+                    onChange={this.handleChange('description')}
+                    margin="normal"
+                    className={classes.FormControl}
+                  />
+
+                </form>
+              </DialogContent>
+              <DialogActions>
+                <Button
+                  onClick={this.handleSubmit}
+                  color="primary"
+                  variant="raised"
+                >
+                   Create 
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </Fragment>
+      )
+    }
   }
-
-  render(){
-    const {open} = this.state;
-    return(
-        <Fragment>
-          <Button variant="fab" onClick={this.handleToggle} mini>
-            <Add/>
-          </Button>
-
-          <Dialog
-            open={open}
-            onClose={this.handleToggle}
-          >
-            <DialogTitle id="form-dialog-title">
-              Create a new exercise
-            </DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                Please fill out the form below
-              </DialogContentText>
-              <form>
-              </form>
-            </DialogContent>
-            <DialogActions>
-              <Button color="primary" variant="raised">
-                 Create 
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </Fragment>
-    )
-  }
-}
+)
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -2,14 +2,17 @@ import React from 'react';
 import {AppBar, Toolbar, Typography} from 'material-ui';
 import CreateDialog from '../Exercises/Dialogs/Create';
 
-export default props => (
+export default ({muscles, onExerciseCreate}) => (
   <AppBar position="static">
     <Toolbar>
       <Typography variant="headline" color="inherit" style={{flex: 1}}>
         Exercise database
       </Typography>
       
-      <CreateDialog/>
+      <CreateDialog
+        onCreate={onExerciseCreate}
+        muscles={muscles}
+      />
     </Toolbar>
   </AppBar>
 );
